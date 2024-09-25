c391ca15d3d6dbdc30a019e3b199765d39e37996
sahilsharma2184 <sahilsharma2184@gmail.com>
2024-09-25 23:23:35 +0530

    initial commit

--- learn.sh
+++ learn.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #this script basically makes a new folder -> inside the new folder creates a new .sh file -> inside the .sh file the '#!/bin/bash' gets written
+#the 6th line 'cat > sahil.sh << EOF' basically redirects the output of cat command to the file sahil.sh, if sahil.sh doesn't exists then a file will be created else the file contents will be over-written in case of it's existence. Further the '<<EOPF' allows multiple lines of text directly into the file
 
 mkdir sahil #new folder
 cd sahil/ #changing directory to sahil
-cat > sahil.sh << EOF #this basically redirects the output of cat command to the file sahil.sh, if sahil.sh doesn't exists then a file will be created else the file contents will be over-written in case of it's existence. Further the '<<EOPF' allows multiple lines of text directly into the file
+cat > sahil.sh << EOF 
 #!/bin/bash     
 EOF
