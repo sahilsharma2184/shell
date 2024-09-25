d5e9ff0e360b2bafd1558d1f207d2433d14c636b
sahilsharma2184 <sahilsharma2184@gmail.com>
2024-09-26 00:15:59 +0530

    initial commit

---
+++ systemuinfo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+####################################################################################
+# Author : Sahil Sharma
+# Version: v1
+# Purpose: To get the information about the CPU, RAM, Storage of the virtual machine
+####################################################################################
+
+echo "The Information abot CPU --->"
+nproc
+
+echo " "
+echo "The Information abot RAM --->"
+free
+
+echo " "
+echo "The Information abot Storage --->"
+df
