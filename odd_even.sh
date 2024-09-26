60e816adf6b9ae0ac2df729388d7bda380d7cbb2
sahilsharma2184 <sahilsharma2184@gmail.com>
2024-09-27 00:07:11 +0530

    initial commit

--- odd_even.sh
+++ odd_even.sh
@@ -1,15 +1,16 @@
+#!/bin/bash
 ####################################################################################
 # Author : Sahil Sharma
 # Version: v1
 # Purpose: To print odd or even numbers between 1 to 10
 ####################################################################################
-#!/bin/bash
+
 set -x
 set -o pipefail
 set -e
 for numbers in {1..10}
 do
-    if [ $(( number % 2 )) -ne 0 ] #This line prints odd numbers, change the 'ne' to 'eq' to print even numbers
+    if [ $(( number % 2 )) -ne 0 ] #This line prints odd numbers, change the 'ne' to 'eq' to print even 
     then
         echo " $number "
     fi
