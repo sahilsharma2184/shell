6b6a83ca7257a91bdd281be04ca7fd0944b55e79
sahilsharma2184 <sahilsharma2184@gmail.com>
2024-09-26 17:24:27 +0530

    initial commit

--- systemuinfo.sh
+++ systemuinfo.sh
@@ -3,9 +3,9 @@
 ####################################################################################
 # Author : Sahil Sharma
 # Version: v1
-# Purpose: To get the information about the CPU, RAM, Storage of the virtual machine
+# Purpose: To get the information about the CPU, RAM, Storage, ID of python processes of the virtual machine
 ####################################################################################
-
+set -x
 echo "The Information abot CPU --->"
 nproc
 
@@ -16,3 +16,7 @@ free
 echo " "
 echo "The Information abot Storage --->"
 df
+
+echo " "
+echo "The IDs of the python processes are --->"
+ps -ef | grep python | awk -F" " '{print $2}'
